2309a78793d962346fc0418071b15d33f1341ef9
joaorezend3 <joaovitorezende45@gmail.com>
2023-02-02 09:59:45 -0300

    separar components/risco de erro

--- src/components/cards/index.tsx
+++ src/components/cards/index.tsx
@@ -6,17 +6,64 @@ const ContainerCards = () => {
             <div>
                 <C.topText>
                     <C.titleCards><h2>Foco</h2></C.titleCards>
-                    <C.exibirCards><a href="">MOSTRAR TUDO</a></C.exibirCards>    
+                    <C.exibirCards><a href="">MOSTRAR TUDO</a></C.exibirCards>
                 </C.topText>
                 <C.cards>
-                <C.card></C.card>
-                <C.card></C.card>
-                <C.card></C.card>
-                <C.card></C.card>
-                <C.card></C.card>
+                    <C.card>
+                        <C.imgCard>
+                            <img src="https://i.scdn.co/image/ab67706f000000020538c13122d3c0a64ceaa279" alt="" />
+                        </C.imgCard>
+                        <C.textCard>
+                            <h2>Charlie Brown</h2>
+                            <p>Banda de rock melodico brasileira.</p>
+                        </C.textCard>
+                    </C.card>
+                    <C.card>
+                        <C.imgCard>
+                            <img src="https://i.scdn.co/image/ab67706f000000020538c13122d3c0a64ceaa279" alt="" />
+                        </C.imgCard>
+                        <C.textCard>
+                            <h2>Charlie Brown</h2>
+                            <p>Banda de rock melodico brasileira.</p>
+                        </C.textCard>
+                    </C.card>
+                    <C.card>
+                        <C.imgCard>
+                            <img src="https://i.scdn.co/image/ab67706f000000020538c13122d3c0a64ceaa279" alt="" />
+                        </C.imgCard>
+                        <C.textCard>
+                            <h2>Charlie Brown</h2>
+                            <p>Banda de rock melodico brasileira.</p>
+                        </C.textCard>
+                    </C.card>
+                    <C.card>
+                        <C.imgCard>
+                            <img src="https://i.scdn.co/image/ab67706f000000020538c13122d3c0a64ceaa279" alt="" />
+                        </C.imgCard>
+                        <C.textCard>
+                            <h2>Charlie Brown</h2>
+                            <p>Banda de rock melodico brasileira.</p>
+                        </C.textCard>
+                    </C.card>
+                    <C.card>
+                        <C.imgCard>
+                            <img src="https://i.scdn.co/image/ab67706f000000020538c13122d3c0a64ceaa279" alt="" />
+                            <C.pleyer>
+                                <C.circle>
+
+                                </C.circle>
+                            </C.pleyer>
+                        </C.imgCard>
+                        <C.textCard>
+                            <h2>Charlie Brown</h2>
+                            <p>Banda de rock melodico brasileira.</p>
+                        </C.textCard>
+                    </C.card>
                 </C.cards>
+
             </div>
         </C.HeaderContent>
+
     )
 }
 export default ContainerCards;
--- src/components/cards/styled.ts
+++ src/components/cards/styled.ts
@@ -15,10 +15,11 @@ align-items: center;
 const titleCards = styled.h2`
 display: inline-block;
 max-width: 100%;
+font-size: 16px;
 text-overflow: ellipsis;
 white-space: nowrap;
 `
-/* Gambiarra */ 
+/* Gambiarra */
 const exibirCards = styled.a`
 margin:0 0 0 900px;
 font-weight:bold;
@@ -34,15 +35,43 @@ a:hover{
 `
 const cards = styled.section`
 display:flex;
+justify-content:center;
 gap:25px;
 `
 const card = styled.div`
 background-color:#161616;
 height:268px;
-width:200px;
+width:170px;
 border-radius:6px;
+box-shadow: rgba(0, 0, 0, 0.25) 0px 14px 28px, rgba(0, 0, 0, 0.22) 0px 10px 10px;
+cursor: pointer;
+padding:5px;
+display:flex;
+flex-direction:column;
+align-items:center;
+:hover{
+    background-color:#232323;
+}
+`
+const imgCard = styled.div`
+overflow: hidden;
+margin-top:10px;
+height: 160px;
+img{
+    height: 100%;
+}
+`
+const textCard = styled.div`
+h2{
+    font-size: 1rem;
+    font-weight:700;
+}
+p{
+    font-size:0.875rem;
+}
+
 `
 
 /* Cards */
 
-export { HeaderContent, topText, titleCards, exibirCards,cards,card };
+export { HeaderContent, topText, titleCards, exibirCards, cards, card, imgCard, textCard };
